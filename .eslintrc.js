51674296ed961467eed425b99ee09e080c6b0cb5
Clarus23 <dnjs3023@gmail.com>
2022-11-21 14:20:24 +0900

    통합 test 가능하도록 수정

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+    amd: true,
+  },
+  extends: ['airbnb-base', 'eslint:recommended'],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'import/no-unresolved': 'off',
+    'no-restricted-globals': 'off',
+    'no-undef': 'off',
+    'no-return-assign': 'off',
+    'no-unused-vars': 'off',
+    'no-plusplus': 'off',
+  },
+};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { RTMClient } = require('@slack/rtm-api');
 const fs = require('fs');
 
 let token;
+const testChannel = 'C04ADSJUK5K';
+const testUID = 'U04BHGBRNE6';
 
 try {
   token = fs.readFileSync('./token').toString('utf-8');
@@ -20,13 +22,30 @@ const square = require('./square');
 rtm.on('message', (message) => {
   const { channel } = message;
   const { text } = message;
-
-  if (!isNaN(text)) {
+  if (message.channel === testChannel) { // 테스트 채널에선 testbot의 말만 들어라.
+    if (message.user === testUID) {
+      if (!isNaN(text)) {
+        square(rtm, text, channel);
+      } else {
+        switch (text) {
+          case 'hi':
+            greeting(rtm, channel);
+            break;
+          case '테스트 채널에서 떠들지 마세요.':
+            break;
+          default:
+            rtm.sendMessage('I am alive', channel);
+        }
+      }
+    }
+  } else if (!isNaN(text)) { // 테스트 채널이 아닌경우 작동방법.
     square(rtm, text, channel);
   } else {
     switch (text) {
       case 'hi':
         greeting(rtm, channel);
+        break;
+      case '테스트 채널에서 떠들지 마세요.':
         break;
       default:
         rtm.sendMessage('I am alive', channel);
