1bb251d3492d2cb6e374dd1c9a30e227800ab10a
Clarus23 <dnjs3023@gmail.com>
2022-11-23 22:38:32 +0900

    과사 단위테스트 추가

--- office.js
+++ office.js
@@ -3,16 +3,23 @@ const officelist = require('./officelist.json');
 // var officeN = Object.keys(officelist);
 
 const office = function (rtm, text, channel, offices) {
-  if (offices) {
-    if (officelist[text] !== undefined) {
-      console.log(officelist[text]);
-      rtm.sendMessage(`${text} 은(는)${officelist[text]}입니다`, channel);
+  try {
+    if (offices) {
+      if (officelist[text] !== undefined) {
+        console.log(officelist[text]);
+        rtm.sendMessage(`${text} 은(는)${officelist[text]}입니다`, channel);
+      } else {
+        rtm.sendMessage('제대로 입력해 주세요', channel);
+      }
     } else {
-      rtm.sendMessage('제대로 입력해 주세요', channel);
+      console.log('학과 사무실 안내합니다');
+      rtm.sendMessage('학과이름을 입력해주세요 (예, Architectural Engineering ) :', channel);
     }
-  } else {
-    console.log('학과 사무실 안내합니다');
-    rtm.sendMessage('학과이름을 입력해주세요 (예, Architectural Engineering ) :', channel);
+
+    return Promise.resolve('success');
+  } catch (error) {
+    console.log('error!', error.data);
+    return Promise.resolve('error');
   }
 };
 module.exports = office;
