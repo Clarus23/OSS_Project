61cb412e447c8f010116248dfe32d64490b592f0
Hyewon_Jung <93754278+Clarus23@users.noreply.github.com>
2022-12-08 21:16:21 +0900

    Update office.js

--- office.js
+++ office.js
@@ -1,21 +1,26 @@
 const officelist = require('./officelist.json');
 
-// var officeN = Object.keys(officelist);
+const officeN = Object.keys(officelist);
 
 const office = function (rtm, text, channel, offices) {
   try {
+    let suc = 0;
+    let tmp = 0;
     if (offices) {
-      if (officelist[text] !== undefined) {
-        console.log(officelist[text]);
-        rtm.sendMessage(`${text} 은(는)${officelist[text]}입니다`, channel);
-      } else {
-        rtm.sendMessage('제대로 입력해 주세요', channel);
+      for (let i = 0; i < officeN.length; i++) {
+        if (officeN[i].toLowerCase().replace(/ /g, '') === text.toLowerCase().replace(/ /g, '')) {
+          tmp = i;
+          rtm.sendMessage(`${text} 은(는) ${officelist[officeN[i]]}에 위치합니다.`, channel);
+          suc = 1;
+        }
+      }
+      if (suc !== 1) {
+        rtm.sendMessage(`${text}을(를) 찾을 수 없습니다.`, channel);
       }
     } else {
       console.log('학과 사무실 안내합니다');
-      rtm.sendMessage('학과이름을 입력해주세요 (예, Architectural Engineering ) :', channel);
+      rtm.sendMessage('학과 이름을 입력해주세요 (예, Architectural Engineering ) :', channel);
     }
-
     return Promise.resolve('success');
   } catch (error) {
     console.log('error!', error.data);
