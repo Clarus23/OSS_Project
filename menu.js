8960420efb01750cf9a9f22b30f365a5d0039526
Lmineck <lmhfox3573@naver.com>
2022-12-08 23:39:16 +0900

    commit with additional function3

--- menu.js
+++ menu.js
@@ -1,36 +1,48 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
-const dt = new Date()
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const dt = new Date();
 
 async function webScraping(url, selector) {
-  let res = []
-  let html = await axios.get(url)
-  let $ = cheerio.load(html.data)
+  const res = [];
+  const html = await axios.get(url);
+  const $ = cheerio.load(html.data);
 
-  for (let v of $(selector)) {
-    res.push($(v).text())
+  for (const v of $(selector)) {
+    res.push($(v).text());
   }
 
   return res;
 }
 
-let today = dt.getDay() + 2
-let url = 'https://sobi.chonbuk.ac.kr/menu/week_menu.php'
-let selector = '#contents > div.contentsArea.WeekMenu > div:nth-child(247) > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child('+today+') > ul > li:nth-last-child(-n + 4)'
-const menu = function(rtm, channel) {
-  switch (today) {
-    case 3: case 4: case 5: case 6: case 7:
-      webScraping(url, selector).then((res)=>{
-        console.log(res)
-        let str = res.join(",")
-        rtm.sendMessage(str, channel)
-      })
-      break
+const today = dt.getDay() + 2;
+const url = 'https://sobi.chonbuk.ac.kr/menu/week_menu.php';
+const selector = `#contents > div.contentsArea.WeekMenu > div:nth-child(247) > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(${today}) > ul > li:nth-last-child(-n + 4)`;
+let mealscore = 1;
 
-    case 2: case 8:
-      rtm.sendMessage('주말엔 진수원에서 식사를 제공하지 않습니다', channel)
-      break
+const menu = async function (rtm, channel) {
+  if (today !== 2 && today !== 8) {
+    webScraping(url, selector).then((res) => {
+      console.log(res);
+      const str = res.join(',');
+      rtm.sendMessage(str, channel);
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].match('가지') || res[i].match('땅콩') || res[i].match('메추리알')) {
+          mealscore -= 1;
+        } else if (res[i].match('고기') || res[i].match('오리') || res[i].match('닭') || res[i].match('돈') || res[i].match('치킨')) {
+          mealscore += 1;
+        }
+      }
+      if (mealscore > 3) {
+        mealscore = 3;
+      } else if (mealscore < 1) {
+        mealscore = 1;
+      }
+      rtm.sendMessage(`오늘의 식단 점수는 ${mealscore}점 입니다.`, channel);
+    });
+  } else {
+    rtm.sendMessage('주말엔 진수원에서 식사를 제공하지 않습니다', channel);
   }
-}
+};
 
-module.exports = menu
+module.exports = menu;
