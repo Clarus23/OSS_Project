9184d53aec0c1d5afdc587400c58ef22e8fec2d0
Clarus23 <dnjs3023@gmail.com>
2022-12-09 19:32:26 +0900

    office add distance

--- office.js
+++ office.js
@@ -1,3 +1,5 @@
+const levenshtein = require('js-levenshtein');
+
 const officelist = require('./officelist.json');
 
 const officeN = Object.keys(officelist);
@@ -16,6 +18,15 @@ const office = function (rtm, text, channel, offices) {
       }
       if (suc !== 1) {
         rtm.sendMessage(`${text}을(를) 찾을 수 없습니다.`, channel);
+
+        const distance = [];
+        let min = 0;
+
+        for (let i = 0; i < officeN.length; i++) {
+          distance[i] = levenshtein(officeN[i].toLowerCase().replace(/ /g, ''), text.toLowerCase().replace(/ /g, ''));
+          if (distance[min] > distance[i]) min = i;
+        }
+        rtm.sendMessage(`${officeN[min]} 은(는) ${officelist[officeN[min]]}에 위치합니다.`, channel);
       }
     } else {
       console.log('학과 사무실 안내합니다');
